2652d076338c3b3e861d7fb5f85fd867cfb6972b
Habba002 <habib.bushra002@gmail.com>
2022-10-07 20:27:05 +0300

    task updated

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,46 +1,43 @@
-import { useEffect, useContext } from "react"
-import { TaskContext } from "../context/task"
+import { useEffect, useContext } from "react";
+import { TaskContext } from "../context/task";
 
-import CreateTask from "../components/task/CreateTask"
-import TaskList from "../components/task/TaskList"
-import UpdateTask from "../components/task/UpdateTask"
+import CreateTask from "../components/task/CreateTask";
+import TaskList from "../components/task/TaskList";
+import UpdateTask from "../components/task/UpdateTask";
 
-import socket from "../socket"
+import socket from "../socket";
 
-import axios from "axios"
+import axios from "axios";
 
-export default function Tasks(){
-
-  const [task, setTask] = useContext(TaskContext)
-
-  useEffect(() => {
-    loadTask()
-  }, [])
-
+export default function Tasks() {
+  const [task, setTask] = useContext(TaskContext);
 
   useEffect(() => {
-    socket.on('new-task', (item) => {
-      setTask((prev) => ({...prev, tasks: [item, ...task.tasks]}))
+    loadTask();
+  }, []);
 
-      return () => socket.off('new-task')
-    })
-  })
+  useEffect(() => {
+    socket.on("new-task", (item) => {
+      setTask((prev) => ({ ...prev, tasks: [item, ...task.tasks] }));
+
+      return () => socket.off("new-task");
+    });
+  });
 
   const loadTask = async () => {
-    try{
-      const { data } = await axios.get("/tasks?page=1")
-      setTask({...task, tasks: data})
-    }catch(err){
-      console.log(err)
+    try {
+      const { data } = await axios.get("/tasks?page=1");
+      setTask({ ...task, tasks: data });
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
-  
   return (
     <>
       <CreateTask />
-      <TaskList />
+        <TaskList />
       <UpdateTask />
     </>
-  )
+  );
 }
